20be35cd979d9f9a70e3d5ba6916f40c78ac7e76
jenlynns <157416964+jenlynns@users.noreply.github.com>
2024-04-28 21:37:47 -0400

    Update spascript.js

--- spa/spascript.js
+++ spa/spascript.js
@@ -1,3 +1,4 @@
 function revealMessage() {
     document.getElementById("hidden message").style.display = 'block';
 }
+// This is the fuction that hides the original message "Happy Birthday". This fuction then reveals the actual message below once clicked.//
